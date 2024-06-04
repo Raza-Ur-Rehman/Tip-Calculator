30caea7e93568ec86495bf0f4a0eb16dadd5f778
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-06-05 02:05:33 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 
 function tipCalculate() {
+
     let billamount = document.getElementById("bill").value;
     let tipamount = document.getElementById("tip").value;
     let total = document.getElementById("total");
-    if ( billamount.value = "") {
-        alert("plese enter your amont");
-    }
-    else {   
+
         let sum =  billamount * (1 + tipamount / 100);
         total.innerText = `${sum}`;
-
 }
